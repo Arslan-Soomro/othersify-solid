90ea377fd2ac44269f421c656e028407653aa154
Arslan <arslanjs.dev@gmail.com>
2023-06-11 15:23:41 +0500

    Add Basic Home and Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Hello from "@/src/components/Hello"
+import Home from "@src/components/Homepage/Home";
+import { Routes, Route } from "@solidjs/router";
 
 function App() {
   return (
     <div>
-      <Hello />
+      <Routes>
+        <Route path="/" component={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div className="flex justify-between p-4">
+      <h3 className="text-xl border-bottom">Othersify</h3>
+      <div className="flex items-center gap-4">
+        <button>Login</button>
+        <button>Sign Up</button>   
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
